4f251192d08f1e063231d1b59924c95a6044c4d5
Claus-Justus Heine <himself@claus-justus-heine.de>
2015-02-20 19:07:40 +0100

    Update for OC8

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,5 +1,17 @@
 <?php
 
+$this->create('dokuwikiembed_root', '/')
+  ->actionInclude('dokuwikiembed/index.php');
+
+$this->create('dokuwikiembed_index', 'index.php')
+  ->actionInclude('dokuwikiembed/index.php');
+
+$this->create('dokuwikiembed_ajax_admin-settings', 'ajax/admin-settings.php')
+  ->actionInclude('dokuwikiembed/ajax/admin-settings.php');
+
+$this->create('dokuwikiembed_ajax_dokuwikiframe', 'ajax/dokuwikiframe.php')
+  ->actionInclude('dokuwikiembed/ajax/dokuwikiframe.php');
+
 OC::$CLASSPATH['DWEMBED\AuthHooks'] = OC_App::getAppPath("dokuwikiembed") . '/lib/auth.php';
 
 $this->create('dokuwikirefresh', '/refresh')->post()->action('DWEMBED\AuthHooks', 'refresh');
